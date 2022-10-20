addbda0255600f1d64fa16006a0612b60c26026a
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-10-20 14:09:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,8 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance""myec2" {
   ami = var.ec2_ami_id
-  instance_type = var.instance_type
+  aws_instance_type = var.instance_type
+ 
 }

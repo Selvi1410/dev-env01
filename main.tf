7b036ec4e34f4dbc009db363d27a502cc0b43ee8
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-10-20 14:30:00 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,24 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-provider "aws" {
-  region = "ap-southeast-1"
-}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
-resource "aws_instance" "myec2" {
-  ami = var.ec2_ami_id
-  instance_type = var.instance_type
+  tags = {
+    Name = "HelloWorld"
+  }
 }
